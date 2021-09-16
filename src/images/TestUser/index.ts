a6c4e091a62b28e9bd89a2cbaa58c8f5377be823
dkqk5154 <dkqk5154@gmail.com>
2021-09-16 18:05:20 +0900

    Fix image config

---
+++ src/images/TestUser/index.ts
@@ -0,0 +1,23 @@
+const info = {
+	source: require('./image.png').default,
+	width: 64,
+	height: 64,
+	up: {
+		sx: 64,
+		sy: 196,
+	},
+	down: {
+		sx: 64,
+		sy: 0,
+	},
+	left: {
+		sx: 0,
+		sy: 128,
+	},
+	right: {
+		sx: 64,
+		sy: 64,
+	},
+};
+
+export default info;
