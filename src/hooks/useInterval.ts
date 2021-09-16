b2103719a4b4c8d066d3151f6777c10f7aa1ba3c
dkqk5154 <dkqk5154@gmail.com>
2021-09-16 18:04:04 +0900

    Fix useinterval custom hook

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,19 +1,21 @@
 import { useRef, useEffect } from 'react';
 
-function useInterval(callback: Function, delay: number) {
-	const savedCallback = useRef(null) as any;
+function useInterval(callback: Function, delay: number | null) {
+	const savedCallback = useRef(null);
 
 	useEffect(() => {
 		savedCallback.current = callback;
-	});
+	}, [callback]);
 
 	useEffect(() => {
 		function tick() {
 			savedCallback.current();
 		}
 
-		let id = setInterval(tick, delay);
-		return () => clearInterval(id);
+		if (delay !== null) {
+			let id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
 	}, [delay]);
 }
 
