879890c26107e46be5d78bd5a310e7c340757cd3
dkqk5154 <dkqk5154@gmail.com>
2021-09-17 16:08:53 +0900

    Add TestBlock config

---
+++ src/images/TestBlock/index.ts
@@ -0,0 +1,15 @@
+const info = {
+	source: require('./image.png').default,
+	width: 48,
+	height: 48,
+	grassBlock: {
+		sx: 0,
+		sy: 0,
+	},
+	grayBlock: {
+		sx: 0,
+		sy: 154,
+	},
+};
+
+export default info;
