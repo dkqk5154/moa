b64dd26bfe1e81d95c0cc56b8270a46aa391ff3f
dkqk5154 <dkqk5154@gmail.com>
2021-09-17 16:09:17 +0900

    Add block infos

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -4,6 +4,13 @@ import styled from 'styled-components';
 import GlobalStyled from 'styles/GlobalStyled';
 import GlobalSidebar from 'components/molecules/GlobalSidebar';
 import Character from 'components/atoms/Character';
+import Block from 'components/atoms/Block';
+
+import { useAppDispatch } from 'app/hooks';
+import { setPosition } from 'components/atoms/Character/characterSlice';
+import { setInfos } from 'components/atoms/Block/blockSlice';
+
+import TestBlock from 'images/TestBlock';
 
 const Styled = {
 	Wrapper: styled(GlobalStyled.Row)`
@@ -20,18 +27,48 @@ const Styled = {
 	`,
 };
 
+const testBlockInfos = new Array(30).fill({
+	x: 0,
+	y: 0,
+	key: '1',
+	imageInfo: {
+		source: TestBlock.source,
+		width: TestBlock.width,
+		height: TestBlock.height,
+		sx: TestBlock.grayBlock.sx,
+		sy: TestBlock.grayBlock.sy,
+	},
+});
+
 const MainPage = (): JSX.Element => {
 	const MapWrapperRef = useRef(null);
 	const [mapContainerInfo, setMapContainerInfo] = useState({
 		width: 0,
 		height: 0,
 	});
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(setPosition({ x: 64, y: 64 }));
+		dispatch(
+			setInfos({
+				infos: testBlockInfos.map((res, i) => {
+					if (i >= 15) {
+						return {
+							...res,
+							y: (i - 15) * res.imageInfo.height,
+						};
+					}
+					return {
+						...res,
+						x: i * res.imageInfo.width,
+					};
+				}),
+			}),
+		);
+	}, [dispatch]);
 
 	const handleResize = () => {
-		console.log({
-			width: MapWrapperRef?.current?.offsetWidth,
-			height: MapWrapperRef?.current?.offsetHeight,
-		});
 		setMapContainerInfo({
 			width: MapWrapperRef?.current?.offsetWidth,
 			height: MapWrapperRef?.current?.offsetHeight,
@@ -53,6 +90,7 @@ const MainPage = (): JSX.Element => {
 				<GlobalSidebar />
 				<Styled.MapWrapper ref={MapWrapperRef}>
 					<Character {...mapContainerInfo} />
+					<Block {...mapContainerInfo} />
 				</Styled.MapWrapper>
 			</GlobalStyled.Container>
 		</Styled.Wrapper>
