54849ab911ebd9153d9727d1300d625af8906074
dkqk5154 <dkqk5154@gmail.com>
2021-09-23 18:09:57 +0900

    Add object envent functions

---
+++ src/utils/objectEvent.ts
@@ -0,0 +1,40 @@
+export const objectPositionCheck = ({ position, size }: ObjectParams) => {
+	return {
+		x1: position.x,
+		y1: position.y,
+		x2: position.x + size.width,
+		y2: position.y,
+		x3: position.x,
+		y3: position.y + size.height,
+		x4: position.x + size.width,
+		y4: position.y + size.height,
+	};
+};
+
+export const isCollision = ({ self, objects }: CollisionParams) => {
+	// console.log('self : ', self);
+	// console.log('objects[0] : ', objects[0]);
+	const result = objects.some((object: ObjectParams) => {
+		const selfPosition = objectPositionCheck(self);
+		const objectPosition = objectPositionCheck(object);
+		const objectCheck =
+			objectPosition.x1 > selfPosition.x4 ||
+			objectPosition.y1 > selfPosition.y4;
+		const selfCheck =
+			selfPosition.x1 > objectPosition.x4 ||
+			selfPosition.y1 > objectPosition.y4;
+
+		return (objectCheck || selfCheck) === false;
+	});
+	return result;
+};
+
+export interface ObjectParams {
+	position: { x: number; y: number };
+	size: { width: number; height: number };
+}
+
+export interface CollisionParams {
+	self: ObjectParams;
+	objects: Array<ObjectParams>;
+}
