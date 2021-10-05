7a25d7701c7b7efb36948039b005371f25267702
dkqk5154 <dkqk5154@gmail.com>
2021-10-05 18:20:48 +0900

    Add button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -5,12 +5,26 @@ import styled from 'styled-components';
 // }
 
 const Button = styled.button`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
 	background-color: var(--gray4);
 	color: var(--white);
 	font-size: var(--font-size6);
 	padding: var(--space3);
 	border: 0px;
 	border-radius: var(--radius2);
+	font-weight: bold;
+	:hover {
+		background-color: var(--gray3);
+	}
+	:active {
+		background-color: var(--gray5);
+	}
+	:focus {
+		outline: none;
+	}
 `;
 
 export default Button;
