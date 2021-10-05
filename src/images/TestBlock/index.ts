83ec5db77b3d5e4e0cdf67d38e9f5003109619f2
dkqk5154 <dkqk5154@gmail.com>
2021-10-05 18:21:20 +0900

    Fix image tile info

--- src/images/TestBlock/index.ts
+++ src/images/TestBlock/index.ts
@@ -2,14 +2,21 @@ const info = {
 	source: require('./image.png').default,
 	width: 48,
 	height: 48,
-	grassBlock: {
-		sx: 0,
-		sy: 0,
-	},
-	grayBlock: {
-		sx: 0,
-		sy: 154,
+	info: {
+		grassBlock: {
+			sx: 0,
+			sy: 0,
+		},
+		grayBlock: {
+			sx: 0,
+			sy: 154,
+		},
 	},
 };
+
+export interface TestBlockInfoProps {
+	sx: number;
+	sy: number;
+}
 
 export default info;
--- src/images/TestTile/index.ts
+++ src/images/TestTile/index.ts
@@ -2,9 +2,11 @@ const info = {
 	source: require('./image.png').default,
 	width: 48,
 	height: 48,
-	grassTile: {
-		sx: 0,
-		sy: 0,
+	info: {
+		grassTile: {
+			sx: 0,
+			sy: 0,
+		},
 	},
 };
 
