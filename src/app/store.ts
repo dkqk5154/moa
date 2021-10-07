3d522d68908382cff24240a3006ddb921ad3e8e3
dkqk5154 <dkqk5154@gmail.com>
2021-10-07 18:54:25 +0900

    Add buildMenuReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import characterReducer from 'components/atoms/Character/characterSlice';
 import blockReducer from 'components/atoms/Block/blockSlice';
 import globalSidebarReducer from 'components/molecules/GlobalSidebar/globalSidebarSlice';
+import buildMenuReducer from 'components/atoms/BuildMenu/buildMenuSlice';
 
 export const store = configureStore({
 	reducer: {
 		character: characterReducer,
 		block: blockReducer,
 		globalSidebarReducer: globalSidebarReducer,
+		buildMenuReducer: buildMenuReducer,
 	},
 });
 
