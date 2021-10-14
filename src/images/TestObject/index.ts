1d6e71b4ca7cabaefdf4880567520b1c029975c4
dkqk5154 <dkqk5154@gmail.com>
2021-10-14 18:48:43 +0900

    Add object

---
+++ src/images/TestObject/index.ts
@@ -0,0 +1,48 @@
+const info = {
+	source: require('./image.png').default,
+	info: {
+		TestObject1: {
+			width: 48,
+			height: 48,
+			up: {
+				sx: 48,
+				sy: 48,
+			},
+			right: {
+				sx: 0,
+				sy: 0,
+			},
+			left: {
+				sx: 0,
+				sy: 0,
+			},
+			down: {
+				sx: 0,
+				sy: 0,
+			},
+		},
+	},
+};
+
+export interface TestBlockInfoProps {
+	width: number;
+	height: number;
+	up: {
+		sx: number;
+		sy: number;
+	};
+	right: {
+		sx: number;
+		sy: number;
+	};
+	left: {
+		sx: number;
+		sy: number;
+	};
+	down: {
+		sx: number;
+		sy: number;
+	};
+}
+
+export default info;
