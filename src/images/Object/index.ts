b55956fc32f236a43a2930301274221d066127e6
dkqk5154 <dkqk5154@gmail.com>
2021-11-29 18:43:28 +0900

    Add object image

--- src/images/Object/index.ts
+++ src/images/Object/index.ts
@@ -3,6 +3,10 @@ const scale1Source = require('./image1.png').default;
 const scale2Source = require('./image2.png').default;
 const scale3Source = require('./image3.png').default;
 
+const lampScale1Source = require('./deco_lamp1.png').default;
+const lampScale2Source = require('./deco_lamp2.png').default;
+const lampScale3Source = require('./deco_lamp3.png').default;
+
 const infos: Array<ImageSourceProps> = [
 	{
 		name: 'woodObject',
@@ -28,6 +32,32 @@ const infos: Array<ImageSourceProps> = [
 		down: {
 			sx: defaultSize * 0,
 			sy: defaultSize * 3,
+		},
+	},
+	{
+		name: 'lampObject',
+		locationName: {
+			kr: '나무 램프',
+			en: 'WoodLamp',
+		},
+		sources: ['', lampScale1Source, lampScale2Source, lampScale3Source],
+		width: defaultSize,
+		height: defaultSize * 2,
+		up: {
+			sx: defaultSize * 0,
+			sy: defaultSize * 1,
+		},
+		right: {
+			sx: defaultSize * 0,
+			sy: defaultSize * 1,
+		},
+		left: {
+			sx: defaultSize * 0,
+			sy: defaultSize * 1,
+		},
+		down: {
+			sx: defaultSize * 0,
+			sy: defaultSize * 1,
 		},
 	},
 ];
