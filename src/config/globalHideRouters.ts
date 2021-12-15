27fd466d0d369842c56e5843b2cd4bc7ed07adb1
dkqk5154 <dkqk5154@gmail.com>
2021-12-15 10:38:49 +0900

    Remove dummy files

--- src/config/globalHideRouters.ts
+++
@@ -1,7 +0,0 @@
-import {loginRouter} from 'config/routerUrl'
-
-const hideRouters : hideRoutersProps = [loginRouter.root.value];
-
-export type hideRoutersProps = Array<String>
-
-export default hideRouters;
